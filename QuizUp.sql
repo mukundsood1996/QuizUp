c044b04462ef369a5144d7738661fa119f3941bd
Vivek-kapoor <vivek.kapoor.2959@gmail.com>
2018-11-24 01:11:15 +0530

    removed unnecessary files

--- QuizUp.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE `QuizUp`.`login_details` ( `unique_id` INT(6) NOT NULL AUTO_INCREMENT , 
-										`name` VARCHAR(50) NOT NULL , 
-										`email_id` TEXT NOT NULL , 
-										`password` VARCHAR(15) NOT NULL , 
-										PRIMARY KEY (`unique_id`)) ENGINE = InnoDB;
