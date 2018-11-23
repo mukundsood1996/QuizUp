6ed55cc8270049be18ad57d4124a62ab5c94f4fa
mathuryash5 <mathuryash5@gmail.com>
2018-11-24 00:50:46 +0530

    modified gitignore

--- database/db_access.py
+++ database/db_access.py
@@ -79,10 +79,12 @@ def connect_db():
     :return: postgres connection object
     """
 
+    """
     connect_str = "dbname='quizup' user='sood' host='localhost' password='sood'"
     # connect_str = "dbname='quizup' user='postgres' host='localhost' password='welcomeback'"
     # connect_str = "dbname='quizup' user='postgres' host='localhost' password='postgres'"
-
+	"""
+	
     try:
         conn = psycopg2.connect(connect_str)
         return conn
